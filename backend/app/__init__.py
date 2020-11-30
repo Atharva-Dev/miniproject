40955088e0b281cb89fe78dab83799528dfb0c19
Atharva-Dev <atharvadev8@gmail.com>
2020-11-30 11:04:57 +0530

    broadcast added

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ import os
 import random
 from flask import Flask, jsonify
 from backend.blockchain.blockchain import Blockchain
+from backend.blockchain.block import Block
 from backend.pubsub import PubSub
 
 
@@ -16,6 +17,16 @@ pubsub =PubSub()
 def default(): 
     return jsonify(chain.to_json())
 
+@app.route('/addblock')
+def addblock():
+    block = Block()
+    block.set_arrival_time()
+    block.set_dispatch_time()
+    chain.add_block(block)
+
+    pubsub.broadcast_block(block)
+
+    return jsonify(chain.to_json())
 
 PORT = 5000
 
--- backend/pubsub.py
+++ backend/pubsub.py
@@ -8,7 +8,10 @@ pnconfig.subscribe_key = 'sub-c-ec69896a-321e-11eb-9d95-7ab25c099cb1'
 pnconfig.publish_key = 'pub-c-c70ece11-d1b1-4c3c-a6a3-94f2147b503b'
 
 
-TEST_CHENNAL = "TEST_CHENNAL"
+CHANNELS = {
+    'TEST':'TEST',
+    'BLOCK':'BLOCK'
+}
 
 
 
@@ -25,11 +28,14 @@ class PubSub():
 
     def __init__(self):
         self.pubnub = PubNub(pnconfig)
-        self.pubnub.subscribe().channels([TEST_CHENNAL]).execute()
+        self.pubnub.subscribe().channels(CHANNELS.values()).execute()
         self.pubnub.add_listener(Listener())
 
     def publish(self, channel, message):
         self.pubnub.publish().channel(channel).message(message).sync()
+
+    def broadcast_block(self, block):
+        self.publish(CHANNELS['BLOCK'], block.to_json())
 
 
 
@@ -38,7 +44,7 @@ class PubSub():
 def main():
     pubsub = PubSub()
     time.sleep(1)
-    pubsub.publish(TEST_CHENNAL, "hi")
+    pubsub.publish(CHANNELS['TEST'], "hi")
     
 
 if __name__ == "__main__":
